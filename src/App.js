4a3ac1383b80583973b31ddd09c0b32451ecf847
DESKTOP-TMB22QD\balin <balint97@yahoo.com>
2022-09-05 08:50:51 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
 		return (
 			<>
 				{this.state.componentDidMount || this.state.currentUser ? (
-					<BrowserRouter basename="/" >
+					<BrowserRouter>
 						<Routes>
 							<Route
 								exact
